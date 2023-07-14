bfdfb12baa08052a02b0380ab59a756017aac6c4
Mike <109431533+1111334@users.noreply.github.com>
2023-07-14 12:09:10 +0200

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,71 @@
+
+public class Test {
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = a + 1;
+
+        a += 5;
+        a -= 4 ;
+        b += 1;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        boolean isShots = b % 2 != 0;
+     // boolean isMultiplyOf3 = (b * (b + 1)) % 3 == 0;
+        boolean isMultiplyOf3 = Math.floorMod(b * (b + 1), 3) == 0 ;
+        System.out.println(isShots);
+        System.out.println(isMultiplyOf3);
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
